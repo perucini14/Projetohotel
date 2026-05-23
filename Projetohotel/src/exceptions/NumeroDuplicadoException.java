22ee1d34d912fcccdcf142607bd407aba5ff69f4
perucini14 <berferreira123@gmail.com>
2026-05-23 00:23:38 -0300

    Add files via upload

---
+++ Projetohotel/src/exceptions/NumeroDuplicadoException.java
@@ -0,0 +1,7 @@
+package exceptions;
+
+public class NumeroDuplicadoException extends Exception {
+    public NumeroDuplicadoException(String mensagem) {
+        super(mensagem);
+    }
+}
---
+++ Projetohotel/src/interfaces/Cadastravel.java
@@ -0,0 +1,7 @@
+package interfaces;
+
+public interface Cadastravel {
+    String getId();
+    String toJson();
+    void exibirDados();
+}
